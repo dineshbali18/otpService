29c10f2717a0eaaf452afdf744c2beb9d1bea330
dineshbali18 <80048098+dineshbali18@users.noreply.github.com>
2022-01-22 17:22:44 +0530

    Update otp.js

--- routes/otp.js
+++ routes/otp.js
@@ -4,7 +4,7 @@ const router=express.Router();
 
 const {generateOtp,verifyOtp,sendOtp,isPresent}=require('../controllers/otp');
 
-router.post("/user/generateotp",isPresent,generateOtp)
+router.post("/user/generateotp",generateOtp)
 router.post("/user/sendotp",sendOtp);
 router.post("/user/verifyotp",verifyOtp);
 

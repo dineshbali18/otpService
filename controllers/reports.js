035aee3672ba69cf0cc8db5832baac6d8ac210e3
dineshbali18 <80048098+dineshbali18@users.noreply.github.com>
2022-03-06 18:17:56 +0530

    Update reports.js

--- controllers/reports.js
+++ controllers/reports.js
@@ -1,1 +1,15 @@
+const mongoose=require("mongoose")
+const reports=require('../models/reports')
 
+exports.createReport=(req,res)=>{
+    // console.log("hi................")
+    const report=new reports(req.body);
+    report.save((err, reportData) => {
+        if (err) {
+          return res.status(400).json({
+            error: "NOT able to save otpData in DB"
+          });
+        }
+        res.json(reportData)
+      });
+}

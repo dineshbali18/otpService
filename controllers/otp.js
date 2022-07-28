7c4ac7ff20e59e193bf87aecb544953c3fdd71df
dineshbali18 <80048098+dineshbali18@users.noreply.github.com>
2022-07-28 13:55:13 +0530

    Update otp.js
    change sendotp method due to google changes

--- controllers/otp.js
+++ controllers/otp.js
@@ -40,30 +40,33 @@ exports.sendOtp=(req,res)=>{
         //   console.log(userOtp);
         send_otp=userOtp[0].otp;
     var transporter = nodemailer.createTransport({
-        service: 'Gmail',
+        service: 'gmail',
         port:465,
-        secure:true,
         auth: {
           user: 'bigboss5teluguvoting@gmail.com',
-          pass: 'DiNeSh5@'
+          pass: 'pcrsarbmhuyhttoq'
         }
       });
+      send();
+      async function send() {
+        const result = await transporter.sendMail({
+            from: 'BigBosstelugu<bigbossvoting@gmail.com>',
+            to: req.body.email,
+            subject: 'Otp for Bigboss telugu',
+            text: `Your Otp is ${send_otp}`
+        });
+    
+        console.log(JSON.stringify(result, null, 4));
+    }
       
-      var mailOptions = {
-        from: 'BigBosstelugu<bigbossvoting@gmail.com>',
-        to: req.body.email,
-        subject: 'Otp for Bigboss 6 telugu',
-        text: `Your Otp is ${send_otp}`
-      };
-      
-      transporter.sendMail(mailOptions, function(error, info){
-          console.log(error);
-        if (error) {
-          console.log(error);
-        } else {
-          console.log('Email sent: ' + info.response);
-        }
-      });
+    //   transporter.sendMail(mailOptions, function(error, info){
+    //       console.log(error);
+    //     if (error) {
+    //       console.log(error);
+    //     } else {
+    //       console.log('Email sent: ' + info.response);
+    //     }
+    //   });
       res.json("otp sent")
     }
     else{
@@ -71,6 +74,52 @@ exports.sendOtp=(req,res)=>{
     }
   })
 }
+
+// exports.sendOtp=(req,res)=>{
+//     //SEND Otp through MAIL
+//     // console.log("sendOtp",req.body.email);
+//     var send_otp;
+//     otp.find({email:req.body.email}).exec((err,userOtp)=>{
+//         if(err){
+//             return res.status(400).json({
+//         error: "Invalid req send otp"
+//       });
+//         }
+//         if(userOtp.length>0){
+//         //   console.log(userOtp);
+//         send_otp=userOtp[0].otp;
+//     var transporter = nodemailer.createTransport({
+//         service: 'Gmail',
+//         port:465,
+//         secure:true,
+//         auth: {
+//           user: 'bigboss5teluguvoting@gmail.com',
+//           pass: 'DiNeSh5@'
+//         }
+//       });
+      
+//       var mailOptions = {
+//         from: 'BigBosstelugu<bigbossvoting@gmail.com>',
+//         to: req.body.email,
+//         subject: 'Otp for Bigboss 6 telugu',
+//         text: `Your Otp is ${send_otp}`
+//       };
+      
+//       transporter.sendMail(mailOptions, function(error, info){
+//           console.log(error);
+//         if (error) {
+//           console.log(error);
+//         } else {
+//           console.log('Email sent: ' + info.response);
+//         }
+//       });
+//       res.json("otp sent")
+//     }
+//     else{
+//         res.json("otp expired")
+//     }
+//   })
+// }
 
 exports.verifyOtp=(req,res)=>{
   console.log(req.body);
